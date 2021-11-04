da5f69aa1510a4ecd8d1460c5aae961c2849974b
Nick Fox-Gieg <nick@fox-gieg.com>
2021-11-04 15:50:44 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
   author = 'Nick Fox-Gieg',                   # Type in your name
   author_email = 'nick@fox-gieg.com',      # Type in your E-Mail
   url = 'https://lightningartist.org',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/LightningArtist/latkpy/archive/v_01.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/LightningArtist/latkpy/archive/refs/tags/0.1.tar.gz',    # I explain this later on
   keywords = ['animation', 'blender', 'drawing'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
       'json',
