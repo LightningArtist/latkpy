a303838504583ef8d444e15771244a7842fd00c2
Nick Fox-Gieg <nick@fox-gieg.com>
2021-11-10 17:07:43 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'latk',         # How you named your package folder (MyLib)
   packages = ['latk'],   # Chose the same as "name"
-  version = '1.0.1',      # Start with a small number and increase it with every change you make
+  version = '1.0.2',      # Start with a small number and increase it with every change you make
   license='apache-2.0',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Lightning Artist Toolkit for volumetric animation',   # Give a short description about your library
   author = 'Nick Fox-Gieg',                   # Type in your name
   author_email = 'nick@fox-gieg.com',      # Type in your E-Mail
   url = 'https://fox-gieg.com',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/LightningArtist/latkpy/archive/refs/tags/1.0.1.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/LightningArtist/latkpy/archive/refs/tags/1.0.2.tar.gz',    # I explain this later on
   keywords = ['animation', 'blender', 'drawing'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
       'numpy'
